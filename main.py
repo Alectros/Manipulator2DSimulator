5855668cc317b906458dd3cc12d82fe5800f167f
Alexandr Baklashov <baclashov.s@yandex.ru>
2022-09-27 23:59:26 +0300

    moved window init to class #1

--- main.py
+++ main.py
@@ -1,73 +1,66 @@
 from PyQt5.QtWidgets import *
 
+class ManipulatorWindow(QWidget):
+        "docs"
+        def __init__(self):
+            super(ManipulatorWindow, self).__init__()
+            self.nodeAngles = [0, 0, 0]
+            self.nodeNames = ["A", "B", "C"]
+            self.edgeLengths = [30, 30, 30]
+            self.edgeNames = ["x1", "x2", "x3"]
+            self.setMinimumSize(700, 500)
+            self.setStyleSheet("QFrame#manipulator { background : rgb(220, 220, 220); border: 1px solid black;}")
+            mainLayout = QHBoxLayout()
+            leftLayout = QVBoxLayout()
+            rightLayout = QHBoxLayout()
+            anglesFormLayout = QFormLayout()
+            angleLabelValue = QLabel("Angle")
+            angleLabelName = QLabel("Name")
+            anglesFormLayout.addRow(angleLabelName, angleLabelValue)
+            self.angleBoxes = [QSpinBox(), QSpinBox(), QSpinBox()]
+            self.angleLabels = [QLabel(self.nodeNames[0]), QLabel(self.nodeNames[1]), QLabel(self.nodeNames[2])]
+            anglesFormLayout.addRow(self.angleLabels[0], self.angleBoxes[0])
+            anglesFormLayout.addRow(self.angleLabels[1], self.angleBoxes[1])
+            anglesFormLayout.addRow(self.angleLabels[2], self.angleBoxes[2])
+            rightLayout.addLayout(anglesFormLayout)
+            self.lengthBoxes = [QSpinBox(), QSpinBox(), QSpinBox()]
+            for ind in range(3):
+                self.angleBoxes[ind].setEnabled(False)
+                self.lengthBoxes[ind].setEnabled(False)
+            lengthsFormLayout = QFormLayout()
+            lengthLabelValue = QLabel("Length")
+            lengthLabelName = QLabel("Name")
+            self.lengthLabels = [QLabel(self.edgeNames[0]), QLabel(self.edgeNames[1]), QLabel(self.edgeNames[2])]
+            lengthsFormLayout.addRow(lengthLabelName, lengthLabelValue)
+            lengthsFormLayout.addRow(self.lengthLabels[0], self.lengthBoxes[0])
+            lengthsFormLayout.addRow(self.lengthLabels[1], self.lengthBoxes[1])
+            lengthsFormLayout.addRow(self.lengthLabels[2], self.lengthBoxes[2])
+            rightLayout.addLayout(lengthsFormLayout)
+            manipulatorFrame = QFrame()
+            manipulatorFrame.setObjectName("manipulator")
+            leftLayout.addWidget(manipulatorFrame, 1)
+            logLabel = QTextBrowser()
+            logLabel.setText("Logs")
+            logLabel.setEnabled(False)
+            logLabel.setMaximumHeight(50)
+            leftLayout.addWidget(logLabel)
+            mainLayout.addLayout(leftLayout, 10)
+            mainLayout.addLayout(rightLayout, 3)
+            self.setLayout(mainLayout)
+            return
+
+        def updateManipulatorData(self):
+            for ind in range(3):
+                self.angleBoxes[ind].setValue(self.edgeLengths[ind])
+                self.lengthBoxes[ind].setValue(self.edgeLengths[ind])
+                self.angleLabels[ind].setText(self.nodeNames[ind])
+                self.lengthLabels[ind].setText(self.edgeNames[ind])
+            return
 
 
 
 app = QApplication([])
-window = QWidget()
-window.setMinimumSize(700, 500)
-window.setStyleSheet("QFrame#manipulator { background : rgb(220, 220, 220); border: 1px solid black;}")
+window = ManipulatorWindow()
 
-nodeNames = ["A", "B", "C"]
-nodeAngles = {0, 0, 0}
-edgeNames = ["x1", "x2", "x3"]
-edgeLengths = {30, 30, 30}
-
-mainLayout = QHBoxLayout()
-leftLayout = QVBoxLayout()
-rightLayout = QHBoxLayout()
-
-
-anglesFormLayout = QFormLayout()
-angleLabelValue = QLabel("Angle")
-angleLabelName = QLabel("Name")
-anglesFormLayout.addRow(angleLabelName, angleLabelValue)
-angleBox1 = QSpinBox()
-angleBox1.setValue(20)
-angleBox1.setEnabled(False)
-angleBox2 = QSpinBox()
-angleBox2.setValue(20)
-angleBox2.setEnabled(False)
-angleBox3 = QSpinBox()
-angleBox3.setValue(20)
-angleBox3.setEnabled(False)
-anglesFormLayout.addRow(nodeNames[0], angleBox1)
-anglesFormLayout.addRow(nodeNames[1], angleBox2)
-anglesFormLayout.addRow(nodeNames[2], angleBox3)
-rightLayout.addLayout(anglesFormLayout)
-
-lengthsFormLayout = QFormLayout()
-lengthLabelValue = QLabel("Length")
-lengthLabelName = QLabel("Name")
-
-lengthBox1 = QSpinBox()
-lengthBox1.setValue(20)
-lengthBox1.setEnabled(False)
-lengthBox2 = QSpinBox()
-lengthBox2.setValue(20)
-lengthBox2.setEnabled(False)
-lengthBox3 = QSpinBox()
-lengthBox3.setValue(20)
-lengthBox3.setEnabled(False)
-lengthsFormLayout.addRow(lengthLabelName, lengthLabelValue)
-lengthsFormLayout.addRow(edgeNames[0], lengthBox1)
-lengthsFormLayout.addRow(edgeNames[1], lengthBox2)
-lengthsFormLayout.addRow(edgeNames[2], lengthBox3)
-rightLayout.addLayout(lengthsFormLayout)
-
-manipulatorFrame = QFrame()
-manipulatorFrame.setObjectName("manipulator")
-
-leftLayout.addWidget(manipulatorFrame, 1)
-logLabel = QTextBrowser()
-logLabel.setText("Logs")
-logLabel.setEnabled(False)
-logLabel.setMaximumHeight(50)
-leftLayout.addWidget(logLabel)
-
-mainLayout.addLayout(leftLayout, 10)
-mainLayout.addLayout(rightLayout, 3)
-
-window.setLayout(mainLayout)
 window.show()
 app.exec()
